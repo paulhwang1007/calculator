79519f1927ba1e8ae0d396db8ab0250133a1ecd8
Paul Hwang <hwpaul1007@gmail.com>
2024-07-29 21:36:44 -0700

    Create calculation operator functions.
    Created the functions for add, subtract, multiply, and divide

---
+++ script.js
@@ -0,0 +1,16 @@
+// Functionality
+function add(firstNum, secondNum) {
+    return firstNum + secondNum;
+}
+
+function subtract(firstNum, secondNum) {
+    return firstNum - secondNum;
+}
+
+function multiply(firstNum, secondNum) {
+    return firstnum * secondNum;
+}
+
+function divide(firstNum, secondNum) {
+    return firstNum / secondNum;
+}
