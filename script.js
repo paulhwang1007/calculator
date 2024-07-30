24252f92f5d99510cc3d51c808170304c88e98aa
Paul Hwang <hwpaul1007@gmail.com>
2024-07-29 21:40:41 -0700

    Create calculator operation variables and function
    Created the initial variables for each input in a calculation and created the operate() function to call one of the operator functions based on what operator is given.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Calculator Operation
+let firstNum = 0;
+let secondNum = 0;
+let operator = "";
+
 // Functionality
 function add(firstNum, secondNum) {
     return firstNum + secondNum;
@@ -14,3 +19,16 @@ function multiply(firstNum, secondNum) {
 function divide(firstNum, secondNum) {
     return firstNum / secondNum;
 }
+
+// Operate()
+function operate(firstNum, operator, secondNum) {
+    if (operator === "+") {
+        add(firstNum, secondNum);
+    } else if (operator === "-") {
+        subtract(firstNum, secondNum);
+    } else if (operator === "*") {
+        multiply(firstNum, secondNum);
+    } else if (operator === "/") {
+        divide(firstNum, secondNum);
+    }
+};
